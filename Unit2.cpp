3d2eaab169ad5e8bc93faf6acc851ae6e621ffff
makas <maksgordiichuk97@gmail.com>
2015-12-02 15:12:35 +0200

    commit with soMedium version

--- Unit2.cpp
+++ Unit2.cpp
@@ -33,31 +33,31 @@ __fastcall TForm2::TForm2(TComponent* Owner)
 	setWord[14]="«гущенка";
 	setWord[15]="";
 
-	imag->LoadFromFile("./level0.bmp");
+//	imag->LoadFromFile(".level0.bmp");
 	Form2->Canvas->StretchDraw(Rect(100,100,200,200),imag);
+
+	while(score < 100) {
+//	Form2->Canvas->Refresh();
+	}
 
 	}
 //---------------------------------------------------------------------------
-
-
-
 void __fastcall TForm2::FormPaint(TObject *Sender)
 {
 	imag->LoadFromFile("./level0.bmp");
-	Form2->Canvas->StretchDraw(Rect(270,100,400,250),imag);
+	Form2->Canvas->StretchDraw(Rect(270,80,440,250),imag);
 
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm2::Button1Click(TObject *Sender)
 {
-	while(score<1500) {
 	static int i=0;
 	if(Edit1->Text==setWord[i]) {
 	 i++;
 	 String FileName="./level"+IntToStr(i)+".bmp";
 	 imag->LoadFromFile(FileName);
-	 Form2->Canvas->StretchDraw(Rect(Form2->Width/2-50, 100, Form2->Width/2+50, 250), imag);
+	 Form2->Canvas->StretchDraw(Rect(Form2->Width/2-165, 50, Form2->Width/2+150, 300), imag);
 //	 Form2->Canvas->StretchDraw(Rect(Form2->Width/2-50,100,Form2->Width/2+50,250),imag);
 	 Edit1->Text="";
 	 score += 100;
@@ -65,10 +65,10 @@ void __fastcall TForm2::Button1Click(TObject *Sender)
 	} else {
 			ShowMessage( "Error, try it now" );
 			}
-	}
-	if( score == 1500 ) {
-	ShowMessage("Congratious! You WIN ;)");
-	Form2->Close(); }
+
+//	if( score == 1500 ) {
+//	ShowMessage("Congratious! You WIN ;)");
+//	Form2->Close(); }
 
 
 
@@ -77,8 +77,8 @@ void __fastcall TForm2::Button1Click(TObject *Sender)
 
 void __fastcall TForm2::FormResize(TObject *Sender)
 {
-Form2->Canvas->CopyRect(Rect(270,100,400,250),Form2->Canvas,Rect(0,0,1,1)) ;
- Form2->Canvas->StretchDraw(Rect(Form2->Width/2-50,100,Form2->Width/2+50,250),imag);
+//Form2->Canvas->CopyRect(Rect(270,100,400,250),Form2->Canvas,Rect(0,0,1,1)) ;
+// Form2->Canvas->StretchDraw(Rect(Form2->Width/2-50,100,Form2->Width/2+50,250),imag);
 
 }
 //---------------------------------------------------------------------------
---
+++ __history/Unit2.dfm.~3~
@@ -0,0 +1,62 @@
+object Form2: TForm2
+  Left = 342
+  Top = 165
+  Anchors = [akLeft, akTop, akRight]
+  Caption = 'Form2'
+  ClientHeight = 501
+  ClientWidth = 716
+  Color = 2849540
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poDesigned
+  OnPaint = FormPaint
+  OnResize = FormResize
+  DesignSize = (
+    716
+    501)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    AlignWithMargins = True
+    Left = 639
+    Top = 40
+    Width = 37
+    Height = 25
+    Anchors = [akTop, akRight]
+    Caption = '0'
+    Color = clYellow
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clYellow
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentColor = False
+    ParentFont = False
+    ExplicitLeft = 636
+  end
+  object Edit1: TEdit
+    Left = 290
+    Top = 400
+    Width = 121
+    Height = 21
+    Align = alCustom
+    Alignment = taCenter
+    Anchors = [akLeft, akTop, akRight]
+    TabOrder = 0
+  end
+  object Button1: TButton
+    Left = 290
+    Top = 427
+    Width = 121
+    Height = 46
+    Anchors = [akLeft, akTop, akRight]
+    Caption = #1042#1074#1077#1089#1090#1080' '#1089#1083#1086#1074#1086
+    Default = True
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+end
